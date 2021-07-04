20eb7298d903f216cdcc8f868c66059ee2a4a511
hmnf <trilesskiyars@gmail.com>
2021-07-04 13:59:58 +0300

    Added new function assertArrays

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php
 
 $a = [7, 8, 9];
-$b = [7, 8, 9];
+$b = [6, 8, 9];
 
-if(count($a) > count($b)){
-    $kolvo = count($a);
-}elseif(count($a) < count($b)){
-    $kolvo = count($b);
-}else{
-    $kolvo = count($a);
-}
-
-$error = null;
-
-for($i = 0; $i < $kolvo; $i++){
-    if($a[$i] !== $b[$i]){
-        $error = true;
-        $break;
+function assertArrays(array $a, array $b){
+    if(count($a) === count($b)){
+        for($i = 0; $i < count($a); $i++){
+            if($a[$i] !== $b[$i]){
+                return false;
+            }
+        }
+        return true;
+    }{
+        return false;
     }
 }
-
-if($error){
-    echo 'разные';
-}else{
+if(assertArrays($a, $b)){
     echo 'одинаковые';
+}else{
+    echo 'разные';
 }
